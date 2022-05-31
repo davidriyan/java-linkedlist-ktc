4585ab3b131e5af688129b8fd2102943cf9757e9
david riyan kurniawan <103322177+davidriyan@users.noreply.github.com>
2022-05-31 18:53:44 +0700

    Add files via upload
    none-

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.project;
+import java.util.LinkedList;
+public class Main{
+    public static void main(String[] args){
+        LinkedList<String> motor = new LinkedList<String>();
+        motor.add("vixion");
+        motor.add("beat");
+        motor.add("aerox");
+        motor.add("mio");
+        motor.add("supra");
+        motor.remove("vixion");
+        System.out.println(motor);
+       
+    }        
+    }
